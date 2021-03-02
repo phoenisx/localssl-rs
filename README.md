2af1b825e6b80a94a0d4375eae5815a9ba141fbc
Shub1427 <shub1493biswas@gmail.com>
2021-03-03 00:23:06 +0530

    feat: read!() macro to read input via stdin

---
+++ README.md
@@ -0,0 +1,17 @@
+# Local SSL Certificate Generator Command Line Tool
+
+This project is a helper command line tool, that
+generates an RSA (DES3 ciphered) private key and an sha256
+encrypted public key.
+
+Help taken from - [freeCodeCamp](https://www.freecodecamp.org/news/how-to-get-https-working-on-your-local-development-environment-in-5-minutes-7af615770eec/)
+
+## Improvements
+
+* Add [Clap](https://github.com/clap-rs/clap) support if needed
+* Add Log Colors, usin [Paris](https://github.com/0x20F/paris).
+
+## References
+
+* Learn About Macros:
+  * List of useful *designators*: https://doc.rust-lang.org/rust-by-example/macros/designators.html
---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use std::{
+    any::type_name,
+    io,
+    io::Write
+};
+use openssl::{
+    rsa::{Rsa},
+    symm::Cipher,
+};
+
+macro_rules! read {
+    ($statement:literal, $return_type:ty) => {{
+        print!("{}: ", $statement);
+        io::stdout().flush().expect("flush failed!");
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read line");
+        input.trim().parse::<$return_type>().unwrap()
+    }};
+}
+
+fn type_of<T>(_: &T) -> &str {
+    type_name::<T>()
+}
+
+fn generate_rsa_key() -> String {
+    let rsa = Rsa::generate(2048).unwrap();
+    let passphrase = "bar";
+    let buffer = rsa
+        .private_key_to_pem_passphrase(Cipher::des_ede3_cbc(), passphrase.as_bytes())
+        .unwrap();
+    let m_str = match String::from_utf8(buffer) {
+        Ok(v) => v,
+        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+    };
+    println!("{:?}", m_str);
+    m_str
+}
+
+fn main() {
+    let input = read!("Input a Number", usize);
+    println!("You Entered: {}", input);
+}

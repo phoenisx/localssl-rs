e92251cd42ec3e0f714d1f109c0210d10a0f30d5
Shub1427 <shub1493biswas@gmail.com>
2021-03-03 09:55:38 +0530

    feat: add write to file method

--- README.md
+++ README.md
@@ -10,6 +10,9 @@ This project is a helper command line tool, that
 generates an RSA (DES3 ciphered) private key and an sha256
 encrypted public key.
 
+> :warning: **NOTE**: This Project requires you to have `openssl` already installed
+> on your OS
+
 Help taken from - [freeCodeCamp](https://www.freecodecamp.org/news/how-to-get-https-working-on-your-local-development-environment-in-5-minutes-7af615770eec/)
 
 ## Improvements
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,14 @@
-use std::{
-    any::type_name,
-    io,
-    io::Write
-};
-use openssl::{
-    rsa::{Rsa},
-    symm::Cipher,
-};
+use openssl::{rsa::Rsa, symm::Cipher};
+use std::{any::type_name, io::{self, Write}, fs};
 
 macro_rules! read {
     ($statement:literal, $return_type:ty) => {{
         print!("{}: ", $statement);
         io::stdout().flush().expect("flush failed!");
         let mut input = String::new();
-        io::stdin().read_line(&mut input).expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
         input.trim().parse::<$return_type>().unwrap()
     }};
 }
@@ -22,7 +17,7 @@ fn type_of<T>(_: &T) -> &str {
     type_name::<T>()
 }
 
-fn generate_rsa_key(passphrase: Option<String>) -> String {
+fn generate_rsa_private_key(passphrase: Option<String>) -> String {
     let rsa = Rsa::generate(2048).unwrap();
     let passphrase = passphrase.unwrap_or("shevy".to_string());
     let buffer = rsa
@@ -36,7 +31,32 @@ fn generate_rsa_key(passphrase: Option<String>) -> String {
     m_str
 }
 
+fn write_to_file(filename: String, data: String) -> io::Result<()> {
+    let mut is_ok = true;
+    let root_dir = "./out";
+    if let Err(err) = fs::create_dir(root_dir) {
+        if !err.kind().eq(&io::ErrorKind::AlreadyExists) {
+            is_ok = false;
+            println!("Directory Create Failed: {:?}", err);
+        }
+    }
+    if is_ok {
+        let mut file = fs::File::create(format!("{}/{}", root_dir, filename))?;
+        file.write_all(data.as_bytes())?;
+        file.sync_data()?;
+    }
+    Ok(())
+}
+
 fn main() {
+    let filename = read!("Enter file basename", String);
     let passphrase = read!("Enter a Passphrase", String);
-    println!("Generated: {}", generate_rsa_key(Some(passphrase)));
+    let private_key = generate_rsa_private_key(Some(passphrase));
+    match write_to_file(format!("{}{}", filename, ".key"), private_key) {
+        Ok(_) => {},
+        Err(err) => {
+            println!("Writing to File Failed: {:?}", err);
+        }
+    }
+
 }
